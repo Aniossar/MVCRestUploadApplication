a5f1f328078ebfbfbc5930c9131a99ecdc387a16
Aniossar <aniossar@yandex.ru>
2022-03-24 16:44:27 +0300

    final

--- src/main/java/com/CalculatorMVCUpload/configuration/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-/*
-package com.CalculatorMVCUpload.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
-*/
