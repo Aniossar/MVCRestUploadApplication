9c6773dfd3662ddb00d773919c7568e31cd85c63
Aniossar <aniossar@yandex.ru>
2022-12-30 20:22:37 +0400

    /me controller added

--- src/main/java/com/CalculatorMVCUpload/controller/AuthController.java
+++ src/main/java/com/CalculatorMVCUpload/controller/AuthController.java
@@ -1,31 +1,41 @@
 package com.CalculatorMVCUpload.controller;
 
+import com.CalculatorMVCUpload.configuration.CustomUserDetails;
+import com.CalculatorMVCUpload.configuration.CustomUserDetailsService;
+import com.CalculatorMVCUpload.configuration.jwt.JwtFilter;
 import com.CalculatorMVCUpload.configuration.jwt.JwtProvider;
 import com.CalculatorMVCUpload.payload.AuthentificationRequest;
-import com.CalculatorMVCUpload.payload.AuthentificationResponce;
+import com.CalculatorMVCUpload.payload.AuthentificationResponse;
+import com.CalculatorMVCUpload.payload.MeResponse;
 import com.CalculatorMVCUpload.payload.RegistrationRequest;
 import com.CalculatorMVCUpload.entity.UserEntity;
 import com.CalculatorMVCUpload.service.UserService;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import java.util.Collection;
+
+import static org.springframework.util.StringUtils.hasText;
 
 
 @RestController
+@NoArgsConstructor
 public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
     @Autowired
     private JwtProvider jwtProvider;
 
-    public AuthController() {
-    }
 
     @PostMapping("/register")
     public String registerUser(@RequestBody @Valid RegistrationRequest registrationRequest) {
@@ -38,10 +48,25 @@ public class AuthController {
     }
 
     @PostMapping("/auth")
-    public AuthentificationResponce auth(@RequestBody AuthentificationRequest request) {
+    public AuthentificationResponse auth(@RequestBody AuthentificationRequest request) {
         UserEntity userEntity = userService.findByLoginAndPassword(request.getLogin(), request.getPassword());
         String token = jwtProvider.generateToken(userEntity.getLogin());
-        return new AuthentificationResponce(token);
+        return new AuthentificationResponse(token);
     }
 
+    @GetMapping("/me")
+    public MeResponse checkUserAuth(@RequestHeader(name = "Authorization") String bearer) {
+        String token = null;
+        if (hasText(bearer) && bearer.startsWith("Bearer ")) {
+            token = bearer.substring(7);
+        }
+        if (token != null && jwtProvider.validateToken(token)) {
+            String userLogin = jwtProvider.getLoginFromToken(token);
+            CustomUserDetails customUserDetails = customUserDetailsService.loadUserByUsername(userLogin);
+
+            //добавить нормальный getRole
+            MeResponse meResponse = new MeResponse(userLogin, "USER");
+            return meResponse;
+        } else return null;
+    }
 }
