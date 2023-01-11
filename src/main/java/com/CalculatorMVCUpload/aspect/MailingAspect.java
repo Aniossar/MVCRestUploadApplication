e8e9b47c3eb653cfff41dde73ceb59cc8aabbd45
Aniossar <aniossar@yandex.ru>
2023-01-11 18:35:30 +0400

    multisession tokens + email registration sender

---
+++ src/main/java/com/CalculatorMVCUpload/aspect/MailingAspect.java
@@ -0,0 +1,47 @@
+package com.CalculatorMVCUpload.aspect;
+
+import com.CalculatorMVCUpload.entity.ActivityEntity;
+import com.CalculatorMVCUpload.entity.EmailContext;
+import com.CalculatorMVCUpload.payload.request.RegistrationRequest;
+import com.CalculatorMVCUpload.service.EmailService;
+import lombok.extern.java.Log;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Aspect
+@Log
+public class MailingAspect {
+
+    @Autowired
+    private EmailService emailService;
+
+    @AfterReturning("execution(* com.CalculatorMVCUpload.controller.AuthController.registerUser(..))"
+            + "&&args(registrationRequest)")
+    public void loginUserAdvice(JoinPoint joinPoint, RegistrationRequest registrationRequest) {
+        try {
+            EmailContext emailContext = new EmailContext();
+            emailContext.setFrom("noreply@koreanika.ru");
+            emailContext.setSubject("Добро пожаловать на портал KOREANIKA");
+            emailContext.setTo(registrationRequest.getEmail());
+            emailContext.setTemplateLocation("greetingLetter");
+            Map<String, Object> context = new HashMap<>();
+            context.put("email", registrationRequest.getEmail());
+            emailContext.setContext(context);
+            emailService.sendMail(emailContext);
+            log.info("Sended welcome letter to email " + registrationRequest.getEmail());
+        } catch (MessagingException e) {
+            log.severe("Error while sending out email — " + e.getLocalizedMessage());
+        }
+    }
+}
--- src/main/java/com/CalculatorMVCUpload/service/EmailService.java
+++ src/main/java/com/CalculatorMVCUpload/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.CalculatorMVCUpload.service;
 
+import com.CalculatorMVCUpload.entity.EmailContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
@@ -7,10 +8,13 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
@@ -18,26 +22,23 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendPlainEmail(String toAddress, String subject, String message) {
+    @Autowired
+    private SpringTemplateEngine templateEngine;
 
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(toAddress);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(message);
-        emailSender.send(simpleMailMessage);
-    }
+    public void sendMail(EmailContext email) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+        Context context = new Context();
+        context.setVariables(email.getContext());
+        String emailContent = templateEngine.process(email.getTemplateLocation(), context);
 
-    public void sendEmailWithAttachment(String toAddress, String subject, String message, String attachment)
-            throws MessagingException, FileNotFoundException {
-
-        MimeMessage mimeMessage = emailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
-        messageHelper.setTo(toAddress);
-        messageHelper.setSubject(subject);
-        messageHelper.setText(message);
-        FileSystemResource file = new FileSystemResource(ResourceUtils.getFile(attachment));
-        messageHelper.addAttachment("File", file);
-        emailSender.send(mimeMessage);
+        mimeMessageHelper.setTo(email.getTo());
+        mimeMessageHelper.setSubject(email.getSubject());
+        mimeMessageHelper.setFrom(email.getFrom());
+        mimeMessageHelper.setText(emailContent, true);
+        emailSender.send(message);
     }
 
 }
