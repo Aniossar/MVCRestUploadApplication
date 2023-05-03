24a089897c0a13dd8604d405e7e3e2ee3f8dd461
Aniossar <aniossar@yandex.ru>
2023-05-03 11:12:46 +0400

    ClientDownloadController выведен из-под auth

--- src/main/java/com/CalculatorMVCUpload/controller/api/ClientDownloadController.java
+++ src/main/java/com/CalculatorMVCUpload/controller/api/ClientDownloadController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/client")
+@RequestMapping("/client")
 public class ClientDownloadController {
 
     @Autowired
